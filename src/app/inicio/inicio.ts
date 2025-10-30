1b9a563eab4d70abb54b42ba81c25607e2a5add7
estepss <quezadaestephanny@gmail.com>
2025-10-30 17:03:52 -0500

    Subida inicial del FrontEnd MoneyMentor

---
+++ src/app/inicio/inicio.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-inicio',
+  imports: [
+
+  ],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css'
+})
+export class Inicio {
+
+}

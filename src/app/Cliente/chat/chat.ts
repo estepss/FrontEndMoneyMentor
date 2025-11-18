c1ad7176e57d74cccd9f265e8f8543b0db004c3a
Nicole Torres <tn564642@gmail.com>
2025-11-17 19:21:56 -0500

    Chat detalle html cliente

--- src/app/Cliente/chat/chat.ts
+++ src/app/Cliente/chat/chat.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChatService } from '../../services/chat-service';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css'
 })
-export class Chat {
+export class ChatComponent {
+  chats: any[] = [];
+  private chatService = inject(ChatService);
+  private router = inject(Router);
 
+  ngOnInit() {
+    const idCliente = Number(localStorage.getItem('idCliente'));
+    if (!idCliente) return;
+
+    this.chatService.listByCliente(idCliente).subscribe({
+      next: (data) => (this.chats = data),
+      error: (err) => console.error('Error al listar chats:', err)
+    });
+  }
+
+  abrirChat(chat: any) {
+    localStorage.setItem('asesorNombre', chat.asesor.nombre);
+    this.router.navigate(['/Chat/Detalle', chat.idChat]);
+  }
+
+  obtenerAvatar(nombre: string): string {
+    return `https://ui-avatars.com/api/?name=${encodeURIComponent(nombre)}&background=ffffff&color=179bae&bold=true`;
+  }
 }

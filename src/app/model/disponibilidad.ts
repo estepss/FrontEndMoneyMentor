2df7938b656d45fbf9105514164be917a5a8b97f
Tu Nombre Completo <victor.sanchez.cr21@gmail.com>
2025-11-24 00:36:12 -0500

    Update validación Disponibilidad Horario

--- src/app/model/disponibilidad.ts
+++ src/app/model/disponibilidad.ts
@@ -1,16 +1,16 @@
-export class Disponibilidad {
-  idDisponibilidad: number;
-  fecha: string;       // Formato: "YYYY-MM-DD"
-  horaInicio: string;  // Formato: "HH:mm" o "HH:mm:ss"
-  horaFin: string;     // Formato: "HH:mm" o "HH:mm:ss"
-  disponible: boolean;
-  idAsesor: number;
+  export class Disponibilidad {
+    idDisponibilidad: number;
+    fecha: string;       // Formato: "YYYY-MM-DD"
+    horaInicio: string;  // Formato: "HH:mm" o "HH:mm:ss"
+    horaFin: string;     // Formato: "HH:mm" o "HH:mm:ss"
+    disponible: boolean;
+    idAsesor: number;
 
-  constructor() {
-    this.fecha = '';
-    this.horaInicio = '';
-    this.horaFin = '';
-    this.disponible = true;
-    this.idAsesor = 0; // O el ID del asesor logueado
-  }
+    constructor() {
+      this.fecha = '';
+      this.horaInicio = '';
+      this.horaFin = '';
+      this.disponible = true;
+      this.idAsesor = 0; // O el ID del asesor logueado
+    }
 }
